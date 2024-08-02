182ed142d73e5981cd06839da71422558e1b06f7
Sam Strike <sam.strike1@gmail.com>
2024-08-02 16:37:34 +0100

    Refactored to repository.

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d685596f4785e87f3772deac580e1da5b65e84f8")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
